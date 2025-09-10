2657f08bfe57a577b42aa5f7618478101dca4783
toino <me@toino.pt>
2025-09-10 03:41:07 +0100

    Enable gamescope session for steam

--- modules/steam/nixos.nix
+++ modules/steam/nixos.nix
@@ -2,6 +2,9 @@
 {
   programs.steam = {
     enable = true;
+
+    gamescopeSession.enable = true;
+
     # Open ports in the firewall for Steam Remote Play
     remotePlay.openFirewall = true;
     # Open ports in the firewall for Source Dedicated Server
