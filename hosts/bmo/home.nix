155eb837552d4d379a12bfbc16076826089c5eeb
toino <me@toino.pt>
2025-11-30 16:13:20 +0000

    Use default scaling for monitors

--- hosts/bmo/home.nix
+++ hosts/bmo/home.nix
@@ -5,8 +5,8 @@
   age.rekey.localStorageDir = ./secrets/home;
 
   wayland.windowManager.hyprland.settings.monitor = [
-    "eDP-2, preferred, auto, 1.60"
-    "desc:LG Electronics LG TV, preferred, auto-left, 1.25"
+    "eDP-2, preferred, auto, 1"
+    "desc:LG Electronics LG TV, preferred, auto-left, 1"
     "desc:Samsung Electric Company LC32G5xT H4ZR703681, preferred, auto-left, 1"
     ", preferred, auto, 1"
   ];
