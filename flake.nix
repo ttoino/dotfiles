16913f6d2e8946defae628c61983f7a978affb4d
toino <me@toino.pt>
2025-12-16 06:34:27 +0000

    Upgrade hyprland

--- flake.nix
+++ flake.nix
@@ -34,7 +34,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     hyprland = {
-      url = "github:hyprwm/Hyprland/v0.52.1";
+      url = "github:hyprwm/Hyprland/v0.52.2";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nix4vscode = {
