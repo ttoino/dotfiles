ddc7774771e2574c52ec31e74e9615e3ce27850c
toino <me@toino.pt>
2025-12-30 20:38:48 +0000

    Re-enable jellyfin desktop

--- modules/gui-apps/home.nix
+++ modules/gui-apps/home.nix
@@ -16,8 +16,7 @@
     gimp3 # Image editor
     hunspell # Spell checker
     hunspellDicts.pt_PT # Portuguese spell checker
-    # FIXME: insecure for now, see https://github.com/NixOS/nixpkgs/issues/437865
-    # jellyfin-media-player # Jellyfin media player
+    jellyfin-desktop # Jellyfin media player
     libreoffice # Office suite
     pavucontrol # Volume control
     vesktop # Discord client
