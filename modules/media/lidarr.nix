bcb0839e268782c97f3977dcb2d75b111e6d448d
toino <me@toino.pt>
2026-03-28 23:25:34 +0000

    Remove unused insecure packages

--- modules/media/lidarr.nix
+++ modules/media/lidarr.nix
@@ -9,9 +9,4 @@
   services.caddy.virtualHosts."music.toino.pt".extraConfig = "reverse_proxy * localhost:8686";
 
   networking.hosts."127.0.0.1" = [ "music.toino.pt" ];
-
-  nixpkgs.config.permittedInsecurePackages = [
-    "aspnetcore-runtime-6.0.36"
-    "dotnet-sdk-6.0.428"
-  ];
 }
