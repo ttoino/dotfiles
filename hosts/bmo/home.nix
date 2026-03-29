4281d01b122f0ac03b17adbf657c9a731706c0a7
toino <me@toino.pt>
2026-03-29 23:15:54 +0100

    Zoom in more on built-in screen

--- hosts/bmo/home.nix
+++ hosts/bmo/home.nix
@@ -5,7 +5,7 @@
   age.rekey.localStorageDir = ./secrets/home;
 
   wayland.windowManager.hyprland.settings.monitor = [
-    "eDP-2, preferred, auto, 1"
+    "eDP-2, preferred, auto, 1.333"
     "desc:LG Electronics LG TV, preferred, auto-left, 1"
     "desc:Samsung Electric Company LC32G5xT H4ZR703681, preferred, auto-left, 1"
     ", preferred, auto, 1"
