94eee779405db5ca725256206cf1912ab0a50e0f
toino <me@toino.pt>
2026-04-05 02:35:06 +0100

    Upgrade packages

--- modules/vscode/home.nix
+++ modules/vscode/home.nix
@@ -168,7 +168,7 @@
         "terminal.integrated.enableMultiLinePasteWarning" = "never";
 
         # Prettier
-        "prettier.prettierPath" = "${pkgs.nodePackages.prettier}/lib/node_modules/prettier";
+        "prettier.prettierPath" = "${pkgs.prettier}/lib/node_modules/prettier";
         "prettier.tabWidth" = 4;
         "prettier.trailingComma" = "all";
         "[css][github-actions-workflow][html][javascript][json][jsonc][markdown][scss][svelte][tailwindcss][typescript][typescriptreact][vue][yaml]" =
