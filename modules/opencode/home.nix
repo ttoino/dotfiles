832ecc3be96caa3c5c4ad07cb0a6afecbb0ac027
toino <me@toino.pt>
2026-04-12 15:30:38 +0100

    Add cloudflare skill to opencode

--- modules/opencode/home.nix
+++ modules/opencode/home.nix
@@ -3,7 +3,13 @@
   programs.opencode = {
     enable = true;
 
-    web.enable = true;
+    web = {
+      enable = true;
+      extraArgs = [
+        "--port=9090"
+        "--hostname=0.0.0.0"
+      ];
+    };
 
     settings = {
       mcp = {
@@ -22,10 +28,6 @@
       provider.opencode-go.options.apiKey = "{file:${
         builtins.replaceStrings [ "\${" ] [ "{env:" ] config.age.secrets.opencode-go-api-key.path
       }}";
-      server = {
-        port = 9090;
-        hostname = "127.0.0.1";
-      };
     };
 
     skills =
@@ -36,10 +38,18 @@
           rev = "98669c11ca63e9c81c11501e1437e5c47b556621";
           hash = "sha256-w//9LB1OVG9jlllY+VDse7Js0dn5x6Ys2vPuQACKsTM=";
         };
+        cloudflare = pkgs.fetchFromGitHub {
+          owner = "cloudflare";
+          repo = "skills";
+          rev = "5ec03da67e230df52b698255c8e5979dc9b124b6";
+          hash = "sha256-OdUnIwWeI2FR1UrHxhkqTigWlK7LeGePF8zbdWnfnEk=";
+        };
       in
       {
+        cloudflare = cloudflare + "/skills/cloudflare";
         frontend-design = anthropic + "/skills/frontend-design";
         skill-creator = anthropic + "/skills/skill-creator";
+        wrangler = cloudflare + "/skills/wrangler";
       };
   };
 
