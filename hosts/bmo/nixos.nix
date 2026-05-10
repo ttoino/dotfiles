5ab43a8822d46e5ff18e554d2822c9a08b270d1d
toino <me@toino.pt>
2026-05-10 18:24:09 +0100

    Move bmo's wireguard config to networkmanager

--- hosts/bmo/nixos.nix
+++ hosts/bmo/nixos.nix
@@ -38,21 +38,53 @@
   age.secrets = {
     wireguard-private-key.rekeyFile = ./wireguard_private_key.age;
     wireguard-prismo-preshared-key.rekeyFile = ../../secrets/wireguard_bmo_prismo_preshared_key.age;
+
+    wireguard-nm-env = {
+      rekeyFile = ./wireguard_nm_env.age;
+      generator = {
+        dependencies = {
+          inherit (config.age.secrets) wireguard-private-key wireguard-prismo-preshared-key;
+        };
+        script =
+          {
+            lib,
+            decrypt,
+            deps,
+            ...
+          }:
+          ''
+            printf 'WIREGUARD_PRIVATE_KEY=%s\n' "$(${decrypt} ${lib.escapeShellArg deps.wireguard-private-key.file} | tr -d '\n')"
+            printf 'WIREGUARD_PSK=%s\n' "$(${decrypt} ${lib.escapeShellArg deps.wireguard-prismo-preshared-key.file} | tr -d '\n')"
+          '';
+      };
+    };
   };
 
-  networking.wg-quick.interfaces.wg0 = {
-    address = [ "10.0.0.2/24" ];
-    dns = [ "10.0.0.1" ];
-    privateKeyFile = config.age.secrets.wireguard-private-key.path;
-    peers = [
-      {
-        # prismo
-        publicKey = "b08qjhwHNUDhDHDHy5CePSDWMH5mVZn73LWGCWZdgHw=";
-        presharedKeyFile = config.age.secrets.wireguard-prismo-preshared-key.path;
-        allowedIPs = [ "10.0.0.1/24" ];
-        persistentKeepalive = 60;
+  networking.networkmanager.ensureProfiles = {
+    environmentFiles = [ config.age.secrets.wireguard-nm-env.path ];
+
+    profiles.prismo = {
+      connection = {
+        id = "prismo";
+        type = "wireguard";
+        interface-name = "wg0";
+        autoconnect = "false";
+      };
+      ipv4 = {
+        address1 = "10.0.0.2/24";
+        dns = "10.0.0.1;";
+        method = "manual";
+        never-default = "true";
+      };
+      ipv6.method = "disabled";
+      wireguard.private-key = "$WIREGUARD_PRIVATE_KEY";
+      "wireguard-peer.b08qjhwHNUDhDHDHy5CePSDWMH5mVZn73LWGCWZdgHw=" = {
         endpoint = "prismo.toino.pt:51820";
-      }
-    ];
+        allowed-ips = "10.0.0.1/24;";
+        persistent-keepalive = 60;
+        preshared-key = "$WIREGUARD_PSK";
+        preshared-key-flags = 0;
+      };
+    };
   };
 }
